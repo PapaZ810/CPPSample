26297ec6f4349b6c3fbcc7bf9d4c814553dac3d1
Zac <zombiezac@att.net>
2019-01-22 13:51:58 -0700

    it prints

---
+++ CPPSample/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CPPSample
+//
+//  Created by Durham, Zachary on 1/22/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
